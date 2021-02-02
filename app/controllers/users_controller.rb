62d7e975520cbd7c0ee506d2f5f1b6b4075adbe0
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-02-02 18:50:47 +0530

    session management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def new
     render "new"
   end
@@ -17,6 +19,7 @@ class UsersController < ApplicationController
     })
 
     if user.save
+      session[:current_user_id] = user.id
       redirect_to "/"
     else
       flash[:error] = user.errors.full_messages.join(", ")
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   root to: "home#index"
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
+  delete "/signout" => "sessions#destroy", as: :destroy_session
 end
