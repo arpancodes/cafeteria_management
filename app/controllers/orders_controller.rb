216d1ad1c9e57e1d523ca3a56e754db5010099ff
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-02-17 01:01:49 +0530

    admin can monitor rhe orders and mark as delivered

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,26 @@
 class OrdersController < ApplicationController
   def index
-    @orders = @current_user.orders
-    render plain: "Order Placed!"
+    if @current_user.role == "Admin"
+      @orders = Order.order(created_at: :desc)
+      render "manage"
+    else
+      @orders = @current_user.orders.order(created_at: :desc)
+      render "index"
+    end
+  end
+
+  def update
+    order = Order.find(params[:id])
+    order.delivered_at = DateTime.now
+    order.save
+    redirect_to orders_path
   end
 
   def create
     order = Order.create!({
       user_id: @current_user.id,
       total_price: @cart.total_price,
+      date: DateTime.now,
     })
     @cart.cart_items.each do |item|
       OrderItem.create!({
@@ -26,6 +39,7 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find_by(id: params[:id], user_id: @current_user.id)
-    render plain: ""
+    @order = Order.find_by(id: params[:id]) if @current_user.role == "Admin"
+    render "show"
   end
 end
